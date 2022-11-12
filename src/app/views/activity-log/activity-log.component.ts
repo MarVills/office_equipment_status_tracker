5daec5ca99d8fe761d81bb69eb55777f439c8f4d
MarVills <villaflormarbenc@gmail.com>
2022-11-12 12:59:25 +0800

    working on reports

--- src/app/views/activity-log/activity-log.component.ts
+++ src/app/views/activity-log/activity-log.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, ViewChild } from '@angular/core';
+
+declare var require: any;
+const data: any = require('src/assets/company.json');
 @Component({
   selector: 'app-activity-log',
   templateUrl: './activity-log.component.html',
   styleUrls: ['./activity-log.component.scss']
 })
-export class ActivityLogComponent implements OnInit {
+export class ActivityLogComponent {
+  editing: any[] = [];
+  rows: any[] = [];
+  temp = [...data];
 
-  constructor() { }
+  loadingIndicator = true;
+  reorderable = true;
 
-  ngOnInit(): void {
+  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
+
+  @ViewChild(ActivityLogComponent, { static: true }) table: ActivityLogComponent = Object.create(null);
+  constructor() {
+    this.rows = data;
+    this.temp = [...data];
+    setTimeout(() => {
+      this.loadingIndicator = false;
+    }, 1500);
   }
 
+  updateFilter(event: any) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table = data;
+  }
+  updateValue(event: any, cell: string, rowIndex: number) {
+    console.log('inline editing rowIndex', rowIndex);
+   // // // this.editing[rowIndex + '-' + cell] = false;
+    this.rows[rowIndex][cell] = event.target.value;
+    this.rows = [...this.rows];
+    console.log('UPDATED!', this.rows[rowIndex][cell]);
+  }
 }
+
--- src/app/views/reports/reports.component.ts
+++ src/app/views/reports/reports.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
+// @Component({
+//   selector: 'app-reports',
+//   templateUrl: './reports.component.html',
+//   styleUrls: ['./reports.component.scss']
+// })
+// export class ReportsComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
+import { Component, ViewChild } from '@angular/core';
+
+declare var require: any;
+const data: any = require('src/assets/company.json');
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.scss']
 })
-export class ReportsComponent implements OnInit {
+export class ReportsComponent {
+  editing: any[] = [];
+  rows: any[] = [];
+  temp = [...data];
 
-  constructor() { }
+  loadingIndicator = true;
+  reorderable = true;
 
-  ngOnInit(): void {
+  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
+
+  @ViewChild(ReportsComponent, { static: true }) table: ReportsComponent = Object.create(null);
+  constructor() {
+    this.rows = data;
+    this.temp = [...data];
+    setTimeout(() => {
+      this.loadingIndicator = false;
+    }, 1500);
   }
 
+  updateFilter(event: any) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table = data;
+  }
+  updateValue(event: any, cell: string, rowIndex: number) {
+    console.log('inline editing rowIndex', rowIndex);
+   // // // this.editing[rowIndex + '-' + cell] = false;
+    this.rows[rowIndex][cell] = event.target.value;
+    this.rows = [...this.rows];
+    console.log('UPDATED!', this.rows[rowIndex][cell]);
+  }
 }
+
+
